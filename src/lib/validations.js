46e6e3e5b85a29749d328818f75f70950866993a
anasghayas <anasghayas2004@gmail.com>
2026-07-06 16:44:47 +0530

    Zod Schema

---
+++ src/lib/validations.js
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const loginSchema = z.object({
+  email: z.string().email('Please enter a valid email address'),
+  password: z.string().min(8, 'Password must be at least 8 characters'),
+})
+export const signupSchema = loginSchema.extend({
+  confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords don't match",
+  path: ["confirmPassword"],
+})
